ac5b5a843664e29b69ce4110160024063307e374
João Pedro <joaopazvdo@gmail.com>
2025-09-10 23:40:14 -0300

    Adaptações para novo padrão de classes e melhoramento do jogo

--- fases/fase2/prototipo.py
+++ fases/fase2/prototipo.py
@@ -1,63 +1,62 @@
 import pygame
-from pygame.display import flip
-from classes import cores
+import sys
+sys.path.append("../../utils")
+from cores import cores
 from classes import Tela
 from classes import Jogador
+from classes import Obstaculo_Fase2
+
+def desenha_jogo(TELA, jogador, obstaculos, perdeu, ganhou):
+    if not perdeu and not ganhou:
+        TELA.tela.fill(TELA.cor)
+        pygame.draw.rect(TELA.tela, jogador.cor, jogador.jogador)
+        for montanha in obstaculos:
+            pygame.draw.rect(TELA.tela, montanha.cor, montanha.obstaculo)
+
+    if perdeu:
+        TELA.tela.fill(cores['preto'])
+        fonte = pygame.font.Font(None, 150)
+        texto = fonte.render('Perdeu', False, cores['branco'])
+        largura_texto, altura_texto = texto.get_size()
+        TELA.tela.blit(texto, ((TELA.LARGURA - largura_texto) // 2, ((TELA.ALTURA - altura_texto) // 2)))
 
 
-class Obstaculo:
-    def __init__(self, x, y, largura, altura, velocidade, cor):
-        self.x = x
-        self.y = y
-        self.largura = largura
-        self.altura = altura
-        self.velocidade = velocidade
-        self.cor = cor
-        self.obstaculo = pygame.Rect(self.x, self.y, self.largura, self.altura)
+def movimentacao(keys, jogador):
+    if keys[pygame.K_SPACE]:
+        jogador.pular()
 
-    def movimentacao(self):
-        self.obstaculo.x -= self.velocidade 
+    jogador.queda_constante()
 
 
-class Jogador_Fase2(Jogador):
-    def __init__(self, x, y, largura, altura, cor, pulo, gravidade):
-        super().__init__(x, y, largura, altura, 0, cor)
-        self.gravidade = gravidade
-        self.pulo = pulo
-        self.jogador = pygame.Rect(self.x, self.y, self.largura, self.altura)
+def cria_lista_obstaculos(num_obstaculos, distancia_entre_obstaculos, TELA):
+    obstaculos = num_obstaculos * [None]
+    x_obstaculo = 100
+    for i in range(num_obstaculos):
+       montanha = Obstaculo_Fase2(x_obstaculo, 35, 20.3, 65, TELA)
+       obstaculos[i] = montanha
+       x_obstaculo += distancia_entre_obstaculos
 
-    def movimento_fase2(self, keys):
-        esta_no_topo = self.jogador.y <= 0
-        if keys[pygame.K_SPACE] and not esta_no_topo:
-            self.velocidade = self.pulo
-
-        self.velocidade += self.gravidade
-        if not esta_no_topo:
-            self.y += self.velocidade
-            self.jogador.y += self.velocidade
-
-
-def desenha_jogo(TELA, jogador, montanha):
-    TELA.tela.fill(TELA.cor)
-    pygame.draw.rect(TELA.tela, jogador.cor, jogador.jogador)
-    pygame.draw.rect(TELA.tela, montanha.cor, montanha.obstaculo)
+    return obstaculos
 
 
 def main():
     pygame.init()
 
     info = pygame.display.Info()
-    TELA = Tela(info.current_w, info.current_h, cores['céu'])
+    TELA = Tela(info.current_w, info.current_h)
+    TELA.cor = cores['ceu']
 
-    jogador = Jogador_Fase2(45, (TELA.ALTURA - 45) // 2, 45, 45, cores['marrom'], (TELA.ALTURA / 600) * -5, (TELA.ALTURA // 600) * 0.25)
-    montanha = Obstaculo(TELA.LARGURA, TELA.ALTURA - TELA.ALTURA * (3/4), 400, TELA.ALTURA * (3/4), 20, cores['marrom'])
-    
+    jogador = Jogador(7.8, 30, 2.3, 4.1, TELA)
+    obstaculos = cria_lista_obstaculos(20, 75 + 20.3, TELA)
+
     clock = pygame.time.Clock()
     FPS = 60
 
+    ganhou = False
+    perdeu = False
     rodando = True
     while rodando:
-        desenha_jogo(TELA, jogador, montanha)
+        desenha_jogo(TELA, jogador, obstaculos, perdeu, ganhou)
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
@@ -66,13 +65,21 @@ def main():
                 if evento.key == pygame.K_ESCAPE:
                     rodando = False
 
+        for montanha in obstaculos:
+            montanha.move_constante_para_esquerda()
+            if montanha.obstaculo.colliderect(jogador.jogador):
+                perdeu = True
+        if jogador.jogador.y <= 0:
+            perdeu = True
+
+        if jogador.jogador.y >= TELA.ALTURA - jogador.altura:
+            perdeu = True
         keys = pygame.key.get_pressed()
-        jogador.movimento_fase2(keys)
-        montanha.movimentacao()
+        movimentacao(keys, jogador)
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
-
     
 
-main()
+if __name__ == '__main__':
+    main()
