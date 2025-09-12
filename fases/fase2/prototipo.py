ee7cddfb6558ba25154544d153b534caf7968827
João Pedro <joaopazvdo@gmail.com>
2025-09-12 16:35:25 -0300

    Protótipo Fase 2 finalizado

--- fases/fase2/prototipo.py
+++ fases/fase2/prototipo.py
@@ -6,19 +6,30 @@ from classes import Tela
 from classes import Jogador
 from classes import Obstaculo_Fase2
 
-def desenha_jogo(TELA, jogador, obstaculos, perdeu, ganhou):
-    if not perdeu and not ganhou:
-        TELA.tela.fill(TELA.cor)
-        pygame.draw.rect(TELA.tela, jogador.cor, jogador.jogador)
-        for montanha in obstaculos:
-            pygame.draw.rect(TELA.tela, montanha.cor, montanha.obstaculo)
+def desenha_jogo(TELA, jogador, obstaculos, planice):
+    TELA.tela.fill(TELA.cor)
+    pygame.draw.rect(TELA.tela, jogador.cor, jogador.jogador)
+    for montanha in obstaculos:
+        pygame.draw.rect(TELA.tela, montanha.cor, montanha.obstaculo)
 
-    if perdeu:
-        TELA.tela.fill(cores['preto'])
-        fonte = pygame.font.Font(None, 150)
-        texto = fonte.render('Perdeu', False, cores['branco'])
-        largura_texto, altura_texto = texto.get_size()
-        TELA.tela.blit(texto, ((TELA.LARGURA - largura_texto) // 2, ((TELA.ALTURA - altura_texto) // 2)))
+    if obstaculos == []:
+        pygame.draw.rect(TELA.tela, planice.cor, planice.obstaculo)
+
+
+def desenha_tela_perdeu(TELA):
+    TELA.tela.fill(cores['preto'])
+    fonte = pygame.font.Font(None, 150)
+    texto = fonte.render('Perdeu', False, cores['branco'])
+    largura_texto, altura_texto = texto.get_size()
+    TELA.tela.blit(texto, ((TELA.LARGURA - largura_texto) // 2, ((TELA.ALTURA - altura_texto) // 2)))
+
+
+def desenha_tela_ganhou(TELA):
+    TELA.tela.fill(cores['preto'])
+    fonte = pygame.font.Font(None, 150)
+    texto = fonte.render('Ganhou', False, cores['branco'])
+    largura_texto, altura_texto = texto.get_size()
+    TELA.tela.blit(texto, ((TELA.LARGURA - largura_texto) // 2, ((TELA.ALTURA - altura_texto) // 2)))
 
 
 def movimentacao(keys, jogador):
@@ -38,6 +49,44 @@ def cria_lista_obstaculos(num_obstaculos, distancia_entre_obstaculos, TELA):
 
     return obstaculos
 
+def mecanica_fase2(TELA, jogador, obstaculos, planice):
+    rodando = True
+    perdeu = False
+    ganhou = False
+
+    keys = pygame.key.get_pressed()
+    movimentacao(keys, jogador)
+
+    for montanha in obstaculos:
+        montanha.move_constante_para_esquerda()
+        if montanha.obstaculo.x + montanha.largura <= 0:
+            obstaculos.remove(montanha)
+
+        if montanha.obstaculo.colliderect(jogador.jogador):
+            perdeu = True
+            rodando = False
+
+    if jogador.jogador.y <= 0:
+        perdeu = True
+        rodando = False
+
+    if jogador.jogador.y >= TELA.ALTURA - jogador.altura:
+        perdeu = True
+        rodando = False
+
+    if obstaculos == []:
+        if jogador.jogador.x < TELA.LARGURA * 60/100:
+            jogador.jogador.x += planice.velocidade
+
+        if planice.obstaculo.x > TELA.LARGURA * 50/100:
+            planice.move_constante_para_esquerda()
+
+    if planice.obstaculo.colliderect(jogador.jogador):
+        ganhou = True
+        rodando = False
+
+    return rodando, ganhou, perdeu
+
 
 def main():
     pygame.init()
@@ -47,16 +96,18 @@ def main():
     TELA.cor = cores['ceu']
 
     jogador = Jogador(7.8, 30, 2.3, 4.1, TELA)
-    obstaculos = cria_lista_obstaculos(20, 75 + 20.3, TELA)
+    obstaculos = cria_lista_obstaculos(1, 75 + 20.3, TELA) 
+    planice = Obstaculo_Fase2(100, 95.9, 50, 4.1, TELA)
 
     clock = pygame.time.Clock()
     FPS = 60
 
+    rodando = True
     ganhou = False
     perdeu = False
-    rodando = True
+
     while rodando:
-        desenha_jogo(TELA, jogador, obstaculos, perdeu, ganhou)
+        desenha_jogo(TELA, jogador, obstaculos, planice)
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
@@ -65,19 +116,34 @@ def main():
                 if evento.key == pygame.K_ESCAPE:
                     rodando = False
 
-        for montanha in obstaculos:
-            montanha.move_constante_para_esquerda()
-            if montanha.obstaculo.colliderect(jogador.jogador):
-                perdeu = True
-        if jogador.jogador.y <= 0:
-            perdeu = True
-
-        if jogador.jogador.y >= TELA.ALTURA - jogador.altura:
-            perdeu = True
-        keys = pygame.key.get_pressed()
-        movimentacao(keys, jogador)
+        rodando, ganhou, perdeu = mecanica_fase2(TELA, jogador, obstaculos, planice)
         pygame.display.flip()
         clock.tick(FPS)
+
+    while perdeu:
+        desenha_tela_perdeu(TELA)
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                perdeu = False
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_ESCAPE:
+                    perdeu = False
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    while ganhou:
+        desenha_tela_ganhou(TELA)
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                ganhou = False
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_ESCAPE:
+                    ganhou = False
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
     pygame.quit()
     
 
